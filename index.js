43e38a7509ce60f0a33bcc79b25a68e7f0753a54
cde-laro <cde-laro@student.42.fr>
2020-07-21 14:43:31 +0200

    Task 2

--- index.js
+++ index.js
@@ -16,13 +16,21 @@ axios.get('https://alivebyacadomia.github.io/headtohead.json')
 })
 
 app.get('/', (req, res) => {
-    res.send("I'm Alive");
+    res.status(200).send("I'm Alive");
 })
 
 app.get('/players', (req, res) => {
-    res.send(players)
+    res.status(200).send(players)
+})
+
+app.get('/players/:id', (req, res) => {
+    let player = players.find(player => player.id == req.params.id)
+    if (player)
+        res.status(200).send(player);
+    else
+        res.status(404).send("404")
 })
 
 app.listen(PORT, function () {
-    console.log('Example app listening on port ' + PORT)
+    console.log('Listening on port ' + PORT)
 })
